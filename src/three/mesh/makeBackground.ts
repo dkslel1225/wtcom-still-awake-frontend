822ae9516c6c18b728cce2b3fdb019569ab9518d
jyn <spdlqjdkslel1226@naver.com>
2025-11-16 00:57:18 +0900

    refactor: 빌딩을 백그라운드에 포함 시킴.
    - 기존: makeRoom 에서 [큐브+창문]을 하나의 방으로 렌더링
    - 변경 후: makeRoom [창문] 렌더링, makeBackground에서 [빌딩건물]을 메쉬 한개로 렌더링
    - 이유: 성능 부하를 줄이기 위함.

--- src/three/mesh/makeBackground.ts
+++ src/three/mesh/makeBackground.ts
@@ -2,38 +2,41 @@ import * as THREE from "three";
 
 export default function makeBackground(scene: THREE.Scene) {
   const floor = makeFloor();
-  const basePart = makeBuildingBasePart();
-  const buildingDoor1 = makeBuildingDoor();
-  const buildingDoor2 = makeBuildingDoor();
-  buildingDoor1.position.x = -0.12;
-  buildingDoor2.position.x = 0.12;
-  basePart.add(buildingDoor1);
-  basePart.add(buildingDoor2);
+  const basePart = makeBuilding();
 
   scene.add(floor);
   scene.add(basePart);
 }
 
 function makeFloor() {
-  const floorGeometry = new THREE.PlaneGeometry(15, 10);
+  const floorGeometry = new THREE.PlaneGeometry(12, 5);
   const floorMaterial = new THREE.MeshStandardMaterial({
     color: 0x808080,
     side: THREE.DoubleSide,
   });
   const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
   floorMesh.rotation.x = -Math.PI / 2; // 바닥을 수평으로 회전
-  floorMesh.position.y = -1.2; // 바닥 위치 설정
+  floorMesh.position.set(-1, -2.5, 0); // 바닥 위치 설정
   return floorMesh;
 }
 
-function makeBuildingBasePart() {
-  const buildingFloor = new THREE.Mesh(
-    new THREE.BoxGeometry(7, 1, 1),
+function makeBuilding() {
+  const building = new THREE.Mesh(
+    new THREE.BoxGeometry(7, 5, 1),
     new THREE.MeshStandardMaterial({ color: 0x1b1418 })
   );
-  buildingFloor.position.y = -1;
+  building.position.x = -1;
 
-  return buildingFloor;
+  const leftDoor = makeBuildingDoor();
+  const rightDoor = makeBuildingDoor();
+
+  leftDoor.position.set(-0.12, -2.25, 0.6);
+  rightDoor.position.set(0.12, -2.25, 0.6);
+
+  building.add(leftDoor);
+  building.add(rightDoor);
+
+  return building;
 }
 
 function makeBuildingDoor() {
@@ -45,7 +48,6 @@ function makeBuildingDoor() {
       emissiveIntensity: 0.8,
     })
   );
-  buildingDoor.position.z = 0.6;
 
   return buildingDoor;
 }
